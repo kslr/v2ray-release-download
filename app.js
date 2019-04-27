02b2ebe5390d0d65f430afeafab156c9510e3e20
Kslr <kslrwang@gmail.com>
2019-04-27 18:15:00 +0800

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const got = require('got');
+const fs = require('fs');
+const p = require('path');
+
+(async () => {
+    let response = await got('https://api.github.com/repos/v2fly/V2FlyBleedingEdgeBinary/releases');
+    let responseBody = JSON.parse(response.body);
+    responseBody[0]['assets'].forEach(function (element) {
+        let name = element.name;
+        let download_url = element.browser_download_url;
+        console.info(`${name}  ${download_url}`);
+
+        got.stream(download_url).pipe(fs.createWriteStream(p.resolve(__dirname, `data/${name}`)));
+    })
+})();
