7f6dfabdc75504bf6ce9e628ce0c9d5ccf91bf91
kslr <kslrwang@gmail.com>
2019-12-29 17:27:49 +0800

    update source

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const p = require('path');
 
 (async () => {
-    let response = await got('https://api.github.com/repos/v2fly/V2FlyBleedingEdgeBinary/releases');
+    let response = await got('https://api.github.com/repos/v2fly/v2ray-core/releases');
     let responseBody = JSON.parse(response.body);
     responseBody[0]['assets'].forEach(function (element) {
         let name = element.name;
